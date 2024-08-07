135deb256d8aefc992e58efac2eec5788e889681
Sergio Zavala <chipx0r@gmail.com>
2024-08-07 16:03:19 -0600

    +landing anterior

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -79,14 +79,77 @@
   <!-- Side bar -->
   <aside class="flex-none md:w-1/3 w-1/3 p-2 pt-3 overflow-auto">
     <div class="flex flex-col space-y-2 h-full" style="background: linear-gradient(180deg, rgba(83,123,213,0.55) 0%, rgba(52,163,206,0.6) 35%, rgba(61,163,197,1) 100%);">
-      {#if placesLibrary && map}
-        <SearchBar bind:location {placesLibrary} {map} initialValue={defaultPlace.name} />
-      {/if}
+      <div>
+        <img src="/logo-tunergia.png" style="width: 20%;" alt="" />
+      </div>
+      <div style="font-size: 28px;font-weight: bold;">
+        ¡Empieza a escribir<br>tu historia solar!
+      </div>
+      <div>Cuenta mas sobre ti</div>
+      
+    {#if placesLibrary && map}
+      <SearchBar bind:location {placesLibrary} {map} initialValue={defaultPlace.name} />
+    {/if}
 
-      {#if location}
-        <Sections {location} {map} {geometryLibrary} {googleMapsApiKey} />
-      {/if}
+    {#if location}
+      <Sections {location} {map} {geometryLibrary} {googleMapsApiKey} />
+    {/if} 
 
+      <!-- <div class="pac-card" id="pac-card">
+        <div id="pac-container">
+           
+        </div>
+       </div> -->
+
+       <br>
+
+       <div>¿Cuándo consumes tu energía?</div>
+       <div>
+        <input type="radio" id="constante" name="tipo" value="constante" />
+            <label for="constante">Durante todo el día</label>
+        <input type="radio" id="diurno" name="tipo" value="diurno" checked />
+            <label for="diurno">Por la mañana</label>
+        <input type="radio" id="nocturno" name="tipo" value="nocturno" />
+            <label for="nocturno">Por la noche</label>
+       </div>
+       <br>
+
+
+       <div>Dinos cuánto pagas al mes en tu factura de luz o tu consumo estimado</div>
+
+       <div>
+
+        <div>
+          <input type="radio" id="euros" name="euroKWH" value="euros" checked />
+              <label for="euros">€/mes</label>
+          <input type="radio" id="kWh" name="euroKWH" value="kWh"/>
+              <label for="kWh">kWh/mes</label>
+        </div>
+        <input type="range" id="slider-range" min="0" max="1000"/>
+        <br>
+        <input type="text" id="tarifa" class="tarifa" value="€50" size="8">
+      </div>
+
+      <!--br>
+      <div>¿Tienes Coche Electrico?</div>
+       <div>
+        <input type="radio" id="si" name="sino" value="si" checked />
+            <label for="si">Si</label>
+        <input type="radio" id="no" name="sino" value="no" />
+            <label for="no">NO</label>
+       </div-->
+
+       <br>
+       <div>¿En qué época del año consumes más?</div>
+       <div>
+        <input type="radio" id="verano" name="mayorconsumo" value="verano" />
+            <label for="verano">Verano</label>
+        <input type="radio" id="invierno" name="mayorconsumo" value="invierno" />
+            <label for="invierno">Invierno</label>
+        <input type="radio" id="constante" name="mayorconsumo" value="constante" checked/>
+            <label for="constante">Durante todo el año es igual</label>
+       </div>
+      
       <div class="grow" />
 
     </div>
